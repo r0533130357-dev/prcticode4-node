c327588b5e68b2be6d5127040b153f62a74f45a3
racheli cohen <r0533130357@gmail.com>
2025-11-23 04:02:38 +0200

    Initial commit

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ app.get('/services', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   res.send('Server is running!');
 });
